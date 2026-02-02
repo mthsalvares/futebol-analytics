4e11fb8ccac126366512203d0606b1cfea492270
Matheus Alvares <alvares.matheus@hotmail.com>
2026-02-01 22:17:12 -0300

    feat: carrega jogadores a partir de arquivo JSON

---
+++ src/data/players_loader.py
@@ -0,0 +1,11 @@
+import json
+from data.player import Jogador
+
+def criaJogador():
+  with open('data/players.json', 'r', encoding='utf-8') as players_json:
+    dados = json.load(players_json)
+
+    jogadores = []
+    for item in dados:
+      jogadores.append(Jogador(item['nome'],item['jogos'],item['gols'],item['assistencias'], item['titulos']))
+    return(jogadores)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from data.player import Jogador
+from data.players_loader import *
 from statistic.player_stats import *
 
 def main():
@@ -7,12 +8,25 @@ def main():
 if __name__ == "__main__":
     main()
 
-jogador = Jogador('Luciano',579,166,45,4)
+'''jogador = Jogador('Luciano',579,166,45,4)
 
 print(f'Jogador: {jogador.nome}\n'
       f'Gols: {jogador.gols}\n'
       f'Assistências: {jogador.assistencias}')
 
 print(mediaGolsJogos(jogador))
-print(mediaAssistJogos(jogador))
+print(mediaAssistJogos(jogador))'''
+
+jogadores = []
+jogadores = criaJogador()
+
+for jogador in jogadores:
+    print(f'Jogador: {jogador.nome}\n'
+      f'Gols: {jogador.gols}\n'
+      f'Assistências: {jogador.assistencias}')
+
+    print(mediaGolsJogos(jogador))
+    print(mediaAssistJogos(jogador))
+
+    print('\n')
 
